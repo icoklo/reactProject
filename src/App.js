9af2a7e429965cd98aa51c8b7d78a35e6c68ffd8
unknown <igor.coklo@gmail.com>
2019-01-24 17:30:44 +0100

    added component which inserts/creates bookmarks in database

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ShowBookmarks from './ShowBookmarks';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CreateBookmarks from './CreateBookmarks';
 
 class App extends Component {
     render() {
@@ -17,7 +18,7 @@ class App extends Component {
                                 <Link to="/bookmarks/show">Prikazi oznake</Link>
                             </li>
                             <li>
-                                <Link to="/topics">Topics</Link>
+                                <Link to="/bookmarks/create">Kreiraj oznake</Link>
                             </li>
                         </ul>
 
@@ -25,7 +26,7 @@ class App extends Component {
 
                         <Route exact path="/" />
                         <Route path="/bookmarks/show" component={ShowBookmarks} />
-                        <Route path="/bookmarks/create" />
+                        <Route path="/bookmarks/create" component={CreateBookmarks} />
                     </div>
                 </Router>
             </div>
--- src/CreateBookmarks.js
+++ src/CreateBookmarks.js
@@ -6,31 +6,67 @@ class CreateBookmarks extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: []
-        }
+            name: '',
+            url: '',
+            description: '',
+            sort_order: null
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    componentDidMount() {
-        fetch(Constant.apiURL + '/bookmarks/all')
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    items: json
-                })
-            })
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        
+        var url = Constant.apiURL + '/bookmarks/store';
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .then(response => console.log('Success:', JSON.stringify(response)))
+            .catch(error => console.error('Error:', error));
     }
 
     render() {
-        var { items } = this.state;
-
         return (
             <div className="CreateBookmarks">
                 <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name:
-                        <input type="name" name="name" required=""  class="form-control">
-                    </label>
-                    <input type="submit" value="Submit" />
+                    <div>
+                        <label>
+                            Naziv:
+                        <input type="text" value={this.state.name} onChange={this.handleChange} name="name" required class="form-control" />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            URL:
+                        <input type="text" name="url" value={this.state.url} onChange={this.handleChange} class="form-control" required />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Opis:
+                            <textarea name="description" value={this.state.description} onChange={this.handleChange} class="form-control">
+                            </textarea>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Poredak:
+                            <input type="number" name="sort_order" value={this.state.sort_order} onChange={this.handleChange} min="1" class="form-control" required />
+                        </label>
+                    </div>
+                    <div>
+                        <input type="submit" value="Submit" />
+                    </div>
                 </form>
             </div>
         );
